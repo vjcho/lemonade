a4e46699a57177f7042d55d85531954d1138af6d
Priscilla <pryu@ucdavis.edu>
2016-05-07 17:51:39 -0700

    angular service, routing, and home state

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,10 +1,32 @@
-var app = angular.module('dashboard', []);
+var app = angular.module('dashboard', ['ui.router']);
 
-app.controller('MainCtrl', ['$scope', function($scope){
-	$scope.posts = [];
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+
+app.controller('MainCtrl', [
+	'$scope', 
+	'posts',
+	function($scope, posts){
+	$scope.posts = posts.posts;
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === '') {return;}
 		$scope.posts.push({title: $scope.title});
 		$scope.title = '';
 	};
-}])
+}]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', {
+        	url: 'home',
+        	templateUrl: '/home.html',
+        	controller: 'MainCtrl'
+      	});
+    $urlRouterProvider.otherwise('home');
+  }]);
