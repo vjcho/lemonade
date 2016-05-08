0f2243fa0fa784d87dc7fdf28442a9fe6d0a6ea4
Priscilla <pryu@ucdavis.edu>
2016-05-08 03:54:02 -0700

    Commented Time

--- models/posts.js
+++ models/posts.js
@@ -2,9 +2,8 @@ var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
 	title: String,
-	time: {type: Date},
+	//time: {type: Date},
 	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
-	time: {type:Date},
 });
 
 mongoose.model('Post', PostSchema);
--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -10,11 +10,10 @@ app.controller('MainCtrl', [
 
 		posts.create({
 			title: $scope.title,
-			time: new Date().getTime(),
+			//time: new Date().getTime(),
 		});
 
 		$scope.title = '';
-		$scope.time = '';
 	};
 
 	/*$scope.incrementUpvotes = function(post){
@@ -85,7 +84,6 @@ app.factory('posts', ['$http', 'auth', function($http, auth){
   	return $http.post('/posts', post, {
   		headers: {Authorization: 'Bearer '+auth.getToken()}
   	}).success(function(data){
-  		//o.posts.push(data);
   		o.posts.unshift(data);
   	});
   };
