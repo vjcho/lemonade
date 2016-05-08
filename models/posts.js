53d4fabeeb215f4ce7abf00e10a530912e2a6579
bnguy <bnguy@ucdavis.edu>
2016-05-08 03:44:46 -0700

    post button

--- models/posts.js
+++ models/posts.js
@@ -4,7 +4,6 @@ var PostSchema = new mongoose.Schema({
 	title: String,
 	time: {type: Date},
 	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
-	time: {type:Date},
 });
 
 mongoose.model('Post', PostSchema);
